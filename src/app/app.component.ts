39527809b30fc7bbad6ef1770c8a03fa1fd692a7
114050_RODI_CARO_Nicolas <114050@tecnicatura.frc.utn.edu.ar>
2024-08-24 13:13:07 -0300

    clase 23 08 24

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AddMessageComponent } from './add-message/add-message.component';
+import { ListMessageComponent } from './list-message/list-message.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AddMessageComponent, ListMessageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'clase_23_08_24';
+  messagesAtTheParent: string[] = []
+
+  addMessage(newMessage : string) {
+    this.messagesAtTheParent.push(newMessage)
+  }
 }
